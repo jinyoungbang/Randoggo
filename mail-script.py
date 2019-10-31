893a4aed9d980dc4aa898b4480f9fe6ad4445438
Jin Young Bang <jybang@bu.edu>
2019-10-30 22:29:01 -0400

    test works

--- mail-script.py
+++ mail-script.py
@@ -7,16 +7,31 @@ from email.message import EmailMessage
 EMAIL_ADDRESS = config.EMAIL_ADDRESS
 EMAIL_PASSWORD = config.EMAIL_PASSWORD
 
+msg = EmailMessage()
+msg['Subject'] = "Your daily dosage of doggos!"
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = "rtchang@bu.edu"
+msg.set_content("Have a great day!")
+
+msg.add_alternative("""\
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <h1>Your daily dosage of doggos!</h1>
+            <img src="https://media.giphy.com/media/4Zo41lhzKt6iZ8xff9/200w_d.gif">
+        </body>
+    </html>
+""", subtype="html")
+
+with open('photo1.png', 'rb') as f:
+    file_data = f.read()
+    file_type = imghdr.what(f.name)
+    file_name = "I love dogs"
+
 with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
     smtp.ehlo()
     smtp.starttls()
     smtp.ehlo()
 
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-
-    subject = 'Your daily dosage of doggos!'
-    body = "Have a great day!"
-
-    msg = f'Subject: {subject}\n\n{body}'
-
-    smtp.sendmail(EMAIL_ADDRESS, 'jybang@bu.edu', msg)
+    smtp.send_message(msg)
