b43086c8dbcc78e2824a973cd1926155ccbf3038
Jin Young Bang <jybang@bu.edu>
2019-10-30 23:18:56 -0400

    Email and GIF works

--- mail-script.py
+++ mail-script.py
@@ -2,36 +2,62 @@ import os
 import smtplib
 import imghdr
 import config
+import requests
 from email.message import EmailMessage
 
 EMAIL_ADDRESS = config.EMAIL_ADDRESS
 EMAIL_PASSWORD = config.EMAIL_PASSWORD
+GIPHY_API_KEY = config.GIPHY_API_KEY
 
-msg = EmailMessage()
-msg['Subject'] = "Your daily dosage of doggos!"
-msg['From'] = EMAIL_ADDRESS
-msg['To'] = "rtchang@bu.edu"
-msg.set_content("Have a great day!")
+try:
+    url = "http://api.giphy.com/v1/gifs/search"
 
-msg.add_alternative("""\
-    <!DOCTYPE html>
-    <html>
-        <body>
-            <h1>Your daily dosage of doggos!</h1>
-            <img src="https://media.giphy.com/media/4Zo41lhzKt6iZ8xff9/200w_d.gif">
-        </body>
-    </html>
-""", subtype="html")
+    querystring = {"api_key":GIPHY_API_KEY,"q":"dogs","rating":"g","limit":"1"}
 
-with open('photo1.png', 'rb') as f:
-    file_data = f.read()
-    file_type = imghdr.what(f.name)
-    file_name = "I love dogs"
+    headers = {
+        'User-Agent': "PostmanRuntime/7.18.0",
+        'Accept': "*/*",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "a9db8ebe-5be8-4fb4-91fd-bfc9a2bc2483,43c266a8-d847-4600-a9d4-4effcdd629bd",
+        'Host': "api.giphy.com",
+        'Accept-Encoding': "gzip, deflate",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache"
+        }
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = response.json()
 
-    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-    smtp.send_message(msg)
+    # URL from GIPHY API
+    doggo_url = response['data'][0]['images']["fixed_width_downsampled"]["url"]
+
+    msg = EmailMessage()
+    msg['Subject'] = "Your daily dosage of doggos!"
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = "jybang1999@gmail.com"
+    msg.set_content("Have a great day!")
+
+    header = "Your daily dosage of doggos!"
+
+    html = """\
+        <!DOCTYPE html>
+        <html>
+            <body>
+                <h1>{header}</h1>
+                <img src="{URL}">
+            </body>
+        </html>
+    """.format(header=header, URL=doggo_url)
+
+    msg.add_alternative(html, subtype="html")
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
+    print("Email sent!")
+except E:
+    print(E)
