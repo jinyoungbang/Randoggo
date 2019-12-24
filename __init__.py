7e0418475c33142beb142e76ae15e6b5c19f28f1
Jin Young Bang <jybang@bu.edu>
2019-12-24 16:39:17 +0900

    added mongodb connection

--- __init__.py
+++ __init__.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
+from pprint import pprint
 import pymongo
+import datetime
+import ssl
+import config
 from pymongo import MongoClient
 
 
-# Try figure out the databases
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/randoggo"
-mongo = PyMongo(app)
+
+client = pymongo.MongoClient(config.MONGO_CLIENT_URL, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
+db = client['email']
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -17,10 +21,20 @@ def home():
 def subscribe():
 	email = request.form['email']
 	print(email)
+	collection = db['email']
+	post = {
+		"email": email,
+		"date": datetime.datetime.utcnow()
+	}
+	x = collection.insert_one(post).inserted_id
+	print(x)
 
-	# Return should render_template
+	# Return should render_template with like "Thank you for subscribing html page"
 	return email
 
+@app.route('/unsubscribe', methods=['GET', 'POST'])
+def unsubscribe_home():
+	return render_template('unsubscribe.html')
 
 
 if __name__ == '__main__':
