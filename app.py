c1fcb6094c79e482f5e47fd00e6275f0a3d160f2
Jin Young Bang <jybang@bu.edu>
2020-01-06 15:47:49 +0900

    test

--- app.py
+++ app.py
@@ -11,80 +11,84 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 # Connecting to MongoDB
-client = pymongo.MongoClient(config.MONGO_CLIENT_URL, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
+# client = pymongo.MongoClient(config.MONGO_CLIENT_URL, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
 
-# Specifying the database
-db = client['email']
+# # Specifying the database
+# db = client['email']
 
-# Regex for email validation
-regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+# # Regex for email validation
+# regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
 
-# Function for validating email
-def check(email):
-	if (re.search(regex, email)):
-		return True
-	else:
-		return False
+# # Function for validating email
+# def check(email):
+# 	if (re.search(regex, email)):
+# 		return True
+# 	else:
+# 		return False
 
-# Flask routes
+# # Flask routes
+# @app.route('/', methods=['GET', 'POST'])
+# def home():
+#     return render_template('index.html')
+
+# @app.route('/result', methods=['POST'])
+# def subscribe():
+# 	email = request.form['email']
+# 	if not check(email):
+# 		# Return render template
+# 		return render_template('invalid-subscribe.html')
+# 	collection = db['email']
+
+# 	# Checks if email exists in the database and converts into boolean value
+# 	email_exists = collection.find(
+# 		{
+# 			"email": email
+# 		}
+# 	).count() > 0
+
+# 	if email_exists:
+# 		return render_template('invalid-subscribe.html')
+# 	else:
+# 		post = {
+# 			"email": email,
+# 			"date": datetime.datetime.utcnow()
+# 		}
+# 		x = collection.insert_one(post).inserted_id
+# 		print(x)
+
+# 		# Return should render_template with like "Thank you for subscribing html page"
+# 		return email
+
+# @app.route('/unsubscribe', methods=['GET', 'POST'])
+# def unsubscribe_home():
+# 	return render_template('unsubscribe.html')
+
+# @app.route('/unsubscribe/result', methods=['POST'])
+# def unsubscribe():
+# 	email = request.form['unsubscribe-email']
+# 	collection = db['email']
+
+# 	# Checks if email exists in the database and converts into boolean value
+# 	email_exists = collection.find(
+# 		{
+# 			"email": email
+# 		}
+# 	).count() > 0
+	
+# 	if email_exists:
+# 		delete = {
+# 			"email": email
+# 		}
+# 		x = collection.delete_one(delete)
+# 		print(x)
+# 		# Return should render_template with like "unsubscribing html page"
+# 		return email
+# 	else:
+# 		return render_template('invalid-unsubscribe.html')
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
-
-@app.route('/result', methods=['POST'])
-def subscribe():
-	email = request.form['email']
-	if not check(email):
-		# Return render template
-		return render_template('invalid-subscribe.html')
-	collection = db['email']
-
-	# Checks if email exists in the database and converts into boolean value
-	email_exists = collection.find(
-		{
-			"email": email
-		}
-	).count() > 0
-
-	if email_exists:
-		return render_template('invalid-subscribe.html')
-	else:
-		post = {
-			"email": email,
-			"date": datetime.datetime.utcnow()
-		}
-		x = collection.insert_one(post).inserted_id
-		print(x)
-
-		# Return should render_template with like "Thank you for subscribing html page"
-		return email
-
-@app.route('/unsubscribe', methods=['GET', 'POST'])
-def unsubscribe_home():
-	return render_template('unsubscribe.html')
-
-@app.route('/unsubscribe/result', methods=['POST'])
-def unsubscribe():
-	email = request.form['unsubscribe-email']
-	collection = db['email']
-
-	# Checks if email exists in the database and converts into boolean value
-	email_exists = collection.find(
-		{
-			"email": email
-		}
-	).count() > 0
-	
-	if email_exists:
-		delete = {
-			"email": email
-		}
-		x = collection.delete_one(delete)
-		print(x)
-		# Return should render_template with like "unsubscribing html page"
-		return email
-	else:
-		return render_template('invalid-unsubscribe.html')
+    return "please work"
 
 if __name__ == '__main__':
    app.run(threaded=True)
