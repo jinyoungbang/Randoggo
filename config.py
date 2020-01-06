698bd6d7523e0c5363b6d375db2d98a89bb7a8b2
Jin Young Bang <jybang@bu.edu>
2020-01-06 15:53:38 +0900

    added config

---
+++ config.py
@@ -0,0 +1,8 @@
+# .gitignore should include reference to config.py
+EMAIL_ADDRESS = "randoggoooo@gmail.com"
+EMAIL_PASSWORD = "WLSdud991023"
+GIPHY_API_KEY = "P8O8tQCSftMTGRgybPLnhZ1oGD8IQVjS"
+
+EMAIL_TO_SEND = "jybang@bu.edu"
+
+MONGO_CLIENT_URL = "mongodb+srv://jybang:WLSdud991023@randoggo-g0c6i.mongodb.net/test?retryWrites=true&w=majority"
